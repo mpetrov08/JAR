4bdff4ec4dd5923ccca846bee5994043ca1c22c3
User1 <User1@MSI>
2024-11-30 00:48:43 +0200

    Add unit tests for UserService

---
+++ JAR.Tests/Mocks/UserManagerMock.cs
@@ -0,0 +1,40 @@
+﻿using JAR.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace JAR.Tests.Mocks
+{
+    public static class UserManagerMock
+    {
+        public static Mock<UserManager<User>> Instance
+        {
+            get
+            {
+                var userStoreMock = new Mock<IUserStore<User>>();
+
+                var userManagerMock = new Mock<UserManager<User>>(
+                    userStoreMock.Object,
+                    null, 
+                    null, 
+                    null, 
+                    null, 
+                    null, 
+                    null, 
+                    null, 
+                    null 
+                );
+
+                userManagerMock.Setup(um => um.FindByIdAsync(It.IsAny<string>()))
+                    .ReturnsAsync((string userId) => new User { Id = userId });
+
+                userManagerMock.Setup(um => um.AddToRoleAsync(It.IsAny<User>(), It.IsAny<string>()))
+                    .ReturnsAsync(IdentityResult.Success);
+
+                userManagerMock.Setup(um => um.RemoveFromRoleAsync(It.IsAny<User>(), It.IsAny<string>()))
+                    .ReturnsAsync(IdentityResult.Success);
+
+                return userManagerMock;
+            }
+        }
+    }
+}
---
+++ JAR.Tests/UnitTests/UserServiceTests.cs
@@ -0,0 +1,88 @@
+﻿using JAR.Core.Contracts;
+using JAR.Core.Services;
+using JAR.Infrastructure.Data.Models;
+using JAR.Tests.Mocks;
+using static JAR.Infrastructure.Constants.AdministratorConstants;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace JAR.Tests.UnitTests
+{
+    [TestFixture]
+    public class UserServiceTests : UnitTestsBase
+    {
+        private IUserService userService;
+        private ILecturerService lecturerService;
+        private Mock<UserManager<User>> userManagerMock;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            userManagerMock = UserManagerMock.Instance; 
+            lecturerService = new LecturerService(repository);
+            userService = new UserService(repository, userManagerMock.Object, lecturerService);
+        }
+
+        [Test]
+        public async Task AllAsync_ShouldWorkCorrectly()
+        {
+            var result = await userService.AllAsync();
+
+            Assert.IsNotNull(result);
+
+            var allConferences = repository
+                .AllReadOnly<User>()
+                .ToList();
+
+            Assert.That(result.Count(), Is.EqualTo(allConferences.Count()));
+
+            var resultUser = result.FirstOrDefault(rc => rc.Id == OwnerUser.Id);
+            var isLecturer = await lecturerService.IsLecturer(OwnerUser.Id);
+
+            Assert.IsNotNull(resultUser);
+            Assert.That(resultUser.Email, Is.EqualTo(OwnerUser.Email));
+            Assert.That(resultUser.FullName, Is.EqualTo(OwnerUser.FirstName + " " + OwnerUser.LastName));
+            Assert.That(resultUser.IsLecturer, Is.EqualTo(isLecturer));
+        }
+
+        [Test]
+        public async Task PromoteUserToAdminAsync_ShouldWorkCorrectly()
+        {
+            var userId = OwnerUser.Id;
+
+            var result = await userService.PromoteUserToAdminAsync(userId);
+
+            Assert.IsTrue(result);
+
+            userManagerMock.Verify(um => um.AddToRoleAsync(It.Is<User>(u => u.Id == userId), AdminRole), Times.Once);
+            userManagerMock.Verify(um => um.FindByIdAsync(It.Is<string>(id => id == userId)), Times.Once);
+        }
+
+        [Test]
+        public async Task RemoveAdminRoleAsync_ShouldWorkCorrectly()
+        {
+            var userId = OwnerUser.Id;
+
+            var userMock = new Mock<User>();
+            userMock.Setup(u => u.Id).Returns(userId); 
+            userMock.Setup(u => u.Email).Returns(OwnerUser.Email); 
+            userMock.Setup(u => u.UserName).Returns(OwnerUser.UserName);
+
+            userManagerMock.Setup(um => um.FindByIdAsync(userId)).ReturnsAsync(userMock.Object);
+
+            var result = await userService.RemoveAdminRoleAsync(userId);
+
+            Assert.IsTrue(result);
+
+            userManagerMock.Verify(um => um.FindByIdAsync(It.Is<string>(id => id == userId)), Times.Exactly(2));
+
+            userManagerMock.Verify(um => um.RemoveFromRoleAsync(It.Is<User>(u => u.Id == userId), AdminRole), Times.Once);
+        }
+    }
+}
